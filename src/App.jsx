ebd4b83e03f7363c36fa6228889fe274a3a8c2b9
BrunoElombo <brunoelombo@gmail.com>
2024-12-05 16:17:10 +0100

    Index page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import './index.css'
+import logo from './assets/bfclogo.png'
 function App() {
   return (
-    <div>
-     
+    <div className="flex flex-row h-screen w-full justify-center items-center">
+      <div>
+        <img src={logo} alt="" className='w-[300px]'/>
+        <p className="text-md text-center bg-green-500 text-white">Frontend starter.</p>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["./src/**/*.{html,js}"],
+    content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
   theme: {
   	extend: {
   		borderRadius: {
