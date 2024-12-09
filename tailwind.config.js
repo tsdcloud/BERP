5a932362e633a0a3dc210b10f022ce3e89cad08b
Wilfread Essomba <wilfreadessomba@gmail.com>
2024-12-09 10:40:20 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ["class"],
     content: [
 		"./index.html",
@@ -85,5 +85,5 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [("tailwindcss-animate")],
+};
