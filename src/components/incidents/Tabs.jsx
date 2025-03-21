d041a5ab9db8e96e9532e8f33a6b890e8d8bd1a3
BrunoElombo <brunoelombo@gmail.com>
2025-03-21 15:26:17 +0100

    fix permissions

--- src/components/incidents/Tabs.jsx
+++ src/components/incidents/Tabs.jsx
@@ -36,49 +36,49 @@ const Tabs = () => {
             isActive:pathname.includes("dashboard") ? true : false,
             link: "/incidents/dashboard",
             requiredPermissions:[],
-            requiredRoles:[]
+            requiredRoles:["maintenance technician", "head guard", "customer manager", "coordinator", "HSE supervisor", "IT technician", "manager"]
         },
         {
             name:"Incidents",
             isActive:pathname === "/incidents" ? true : false,
             link: "/incidents",
             requiredPermissions:[],
-            requiredRoles:[]
+            requiredRoles:["maintenance technician", "head guard", "customer manager", "coordinator", "HSE supervisor", "IT technician", "manager"]
         },
         {
             name:"Hors pont",
             isActive: pathname.includes("off-bridge") ? true : false,
             link: "/incidents/off-bridge",
             requiredPermissions:["incident__view_off_bridges"],
-            requiredRoles:[]
+            requiredRoles:["maintenance technician", "head guard", "coordinator", "manager"]
         },
         {
             name:"Maintenances",
             isActive:(pathname.includes("maintenance") && !pathname.includes("type")) ? true : false,
             link: "/incidents/maintenance",
-            requiredPermissions:["incident__view_maintenance"],
-            requiredRoles:[]
+            requiredPermissions:[""],
+            requiredRoles:["maintenance technician", "HSE supervisor", "IT technician", "manager"]
         },
         {
             name:"Causes d'incidents",
             isActive:pathname.includes("incident-cause") ? true : false,
             link: "/incidents/incident-cause",
             requiredPermissions:["incident__view_incident_causes"],
-            requiredRoles:[]
+            requiredRoles:["IT technician"]
         },
         {
             name:"Types d'incidents",
             isActive:pathname.includes("incident-type") ? true : false,
             link: "/incidents/incident-type",
             requiredPermissions:["incident__view_incident_types"],
-            requiredRoles:[]
+            requiredRoles:["IT technician"]
         },
         {
             name:"Type de Maintenances",
             isActive:pathname.includes("maintenance-type") ? true : false,
             link: "/incidents/maintenance-type",
             requiredPermissions:["incident__view_maintenance_types"],
-            requiredRoles:[]
+            requiredRoles:["IT technician"]
         },
         // {
         //     name:"Consommables",
@@ -90,7 +90,7 @@ const Tabs = () => {
             isActive:pathname.includes("equipement") ? true : false,
             link: "/incidents/equipement",
             requiredPermissions:["incident__view_equipements"],
-            requiredRoles:[]
+            requiredRoles:["IT technician"]
         }
     ]
 
@@ -103,7 +103,7 @@ const Tabs = () => {
             <div className='flex flex-col gap-2'>
                 {
                     links.map((link, index) => 
-                        (link.requiredPermissions.every(permission => userPermissions.includes(permission)) || link.requiredRoles.every(role => userRoles.includes(role))) &&
+                            (link.requiredPermissions.filter(permission => userPermissions.includes(permission)).length !==0 || link.requiredRoles.filter(role => userRoles.includes(role))).length !== 0 &&
                         <div key={index} className={`px-2 p-1 ${link?.isActive ? "bg-secondary text-white" : "border-[1px] border-gray-300"} rounded-full cursor-pointer text-sm font-semibold flex justify-center`} onClick={()=>navigate(link?.link)}><span>{link?.name}</span></div>
                     )
                 }
@@ -112,7 +112,7 @@ const Tabs = () => {
         <div className='hidden md:flex gap-2 items-center whitespace-nowrap overflow-x-auto no-scrollbar'>
             {
                 links.map((link, index) => 
-                    (link.requiredPermissions.every(permission => userPermissions.includes(permission)) || link.requiredRoles.every(role => userRoles.includes(role))) &&
+                    (link.requiredPermissions.filter(permission => userPermissions.includes(permission)).length !==0 || link.requiredRoles.filter(role => userRoles.includes(role))).length !== 0 &&
                 <div key={index} className={`px-2 p-1 ${link?.isActive ? "bg-secondary text-white" : "border-[1px] border-gray-300"} rounded-full cursor-pointer text-sm font-semibold flex justify-center`} onClick={()=>navigate(link?.link)}><span>{link?.name}</span></div>
             )
             }
