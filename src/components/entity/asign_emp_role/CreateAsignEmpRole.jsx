b2dba9a506dadb7bd4ca0b9a477a0f3f1f31de3d
Wilfread Essomba <wilfreadessomba@gmail.com>
2025-04-17 17:52:20 +0100

    fixed at createAsignEmpRole

--- src/components/entity/asign_emp_role/CreateAsignEmpRole.jsx
+++ src/components/entity/asign_emp_role/CreateAsignEmpRole.jsx
@@ -231,10 +231,10 @@ export default function CreateAsignEmpRole({setOpen, onSubmit}) {
 
                     <div className='my-3'>
                             <h6 className='text-xs'>Attribuer un ou plusieurs rôles</h6>
-                            <div className='flex flex-wrap my-2 overflow-y-auto h-[300px]'>
+                            <div className='flex flex-wrap items-center overflow-y-auto h-[100px]'>
                                 {showRole.map(item => (
                                     <div key={item?.id} 
-                                    className={`flex font-mono items-center ml-2 mb-2 px-2 py-2 border bg-secondary text-white rounded-sm
+                                    className={`flex font-mono items-center ml-1 px-2 py-2 border bg-secondary text-white rounded-sm
                                     ${errors.roleId ? "border-red-500" : "border-gray-300"}`}>
                                     <input
                                       type="checkbox"
