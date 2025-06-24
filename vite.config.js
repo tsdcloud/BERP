efbced8e6060c40dd0300716ce12532d18d0f9a7
BrunoElombo <brunoelombo@gmail.com>
2025-06-24 08:44:23 +0100

    240625_0844

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [react()],
