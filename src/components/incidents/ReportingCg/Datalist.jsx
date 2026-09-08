1c4abaf7b085d9982a0e77f6d2c2cf7832165568
EmmanuelBFC <eaoudou@bfcgroupsa.com>
2026-09-08 12:22:01 +0100

    Correction of RCG labels

--- src/components/incidents/ReportingCg/Datalist.jsx
+++ src/components/incidents/ReportingCg/Datalist.jsx
@@ -585,11 +585,11 @@ const Datalist = ({
           const exists = hasWatchReport(record);
           return exists ? (
             <span className="inline-flex items-center rounded-full bg-green-100 px-2.5 py-0.5 text-xs font-semibold text-green-800">
-              Présent
+              Traité 
             </span>
           ) : (
             <span className="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-semibold text-red-800">
-              Absent
+              Transmis
             </span>
           );
         },
